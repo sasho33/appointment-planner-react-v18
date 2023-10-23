9823fca40a039094c7722e51d88b9d575e04c81f
sasho33 <sasho3333333@gmail.com>
2023-10-23 07:29:52 +0100

    add contact page created

--- src/components/contactForm/ContactForm.js
+++ src/components/contactForm/ContactForm.js
@@ -1,16 +1,39 @@
-import React from "react";
+import React from 'react';
 
 export const ContactForm = ({
   name,
   setName,
   phone,
-  setPhone,
+  setPhoneNumber,
   email,
   setEmail,
-  handleSubmit
+  handleSubmit,
 }) => {
   return (
-    <></>
+    <>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Name:
+          <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+        </label>
+
+        <label>
+          Phone:
+          <input
+            type="text"
+            value={phone}
+            onChange={(e) => setPhoneNumber(e.target.value)}
+            pattern="[0-9]{10}" // You can adjust the pattern to your preferred phone locale
+          />
+        </label>
+
+        <label>
+          Email:
+          <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+        </label>
+
+        <button type="submit">Submit</button>
+      </form>
+    </>
   );
 };
-
--- src/containers/contactsPage/ContactsPage.js
+++ src/containers/contactsPage/ContactsPage.js
@@ -1,13 +1,23 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
 
-import { ContactForm } from "../../components/contactForm/ContactForm";
-import { TileList } from "../../components/tileList/TileList";
+import { ContactForm } from '../../components/contactForm/ContactForm';
+import { TileList } from '../../components/tileList/TileList';
 
-export const ContactsPage = () => {
+export const ContactsPage = ({ contacts, addContact }) => {
   /*
   Define state variables for 
   contact info and duplicate check
   */
+  const [name, setName] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [email, setEmail] = useState('');
+  const [dublicateCheck, setDublicateCheck] = useState(false);
+
+  useEffect(() => {
+    // Check for duplicate names
+    const isDublicate = contacts.some((contact) => contact.name === name);
+    setDublicateCheck(isDublicate);
+  }, [name, contacts]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -15,6 +25,12 @@ export const ContactsPage = () => {
     Add contact info and clear data
     if the contact name is not a duplicate
     */
+    if (!dublicateCheck) {
+      addContact(name, phoneNumber, email);
+    }
+    setName('');
+    setPhoneNumber('');
+    setEmail('');
   };
 
   /*
@@ -25,7 +41,17 @@ export const ContactsPage = () => {
   return (
     <div>
       <section>
-        <h2>Add Contact</h2> 
+        <h2>Add Contact</h2>
+        <ContactForm
+          name={name}
+          phoneNumber={phoneNumber}
+          email={email}
+          setName={setName}
+          setPhoneNumber={setPhoneNumber}
+          setEmail={setEmail}
+          handleSubmit={handleSubmit}
+          duplicateCheck={dublicateCheck}
+        />
       </section>
       <hr />
       <section>
